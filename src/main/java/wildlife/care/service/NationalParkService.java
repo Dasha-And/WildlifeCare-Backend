1e522bc52aade1e4a4f3c4c19ef019df948461ef
Dasha-And <dosados69@gmail.com>
2021-11-13 15:28:01 +0200

    implemented services for worker and national park

--- src/main/java/wildlife/care/service/NationalParkService.java
+++ src/main/java/wildlife/care/service/NationalParkService.java
@@ -1,4 +1,35 @@
 package wildlife.care.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wildlife.care.model.NationalPark;
+import wildlife.care.repository.NationalParkRepository;
+
+import java.util.List;
+
+@Service
 public class NationalParkService {
+
+    @Autowired
+    NationalParkRepository nationalParkRepository;
+
+    public List<NationalPark> findAll() {
+        return nationalParkRepository.findAll();
+    }
+
+    public NationalPark findById(int id) {
+        return nationalParkRepository.findById(id);
+    }
+
+    public NationalPark insertNationalPark(NationalPark nationalPark) {
+        return nationalParkRepository.save(nationalPark);
+    }
+
+    public NationalPark updateNationalPark(NationalPark nationalPark) {
+        return nationalParkRepository.save(nationalPark);
+    }
+
+    public void deleteNationalPark(int id) {
+        nationalParkRepository.deleteById(id);
+    }
 }
--- src/main/java/wildlife/care/service/WorkerService.java
+++ src/main/java/wildlife/care/service/WorkerService.java
@@ -1,4 +1,35 @@
 package wildlife.care.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wildlife.care.model.Worker;
+import wildlife.care.repository.WorkerRepository;
+
+import java.util.List;
+
+@Service
 public class WorkerService {
+
+    @Autowired
+    WorkerRepository workerRepository;
+
+    public List<Worker> findAll() {
+        return workerRepository.findAll();
+    }
+
+    public Worker findById(int id) {
+        return workerRepository.findById(id);
+    }
+
+    public Worker insertWorker(Worker worker) {
+        return workerRepository.save(worker);
+    }
+
+    public Worker updateWorker(Worker worker) {
+        return workerRepository.save(worker);
+    }
+
+    public void deleteWorker(int id) {
+        workerRepository.deleteById(id);
+    }
 }
