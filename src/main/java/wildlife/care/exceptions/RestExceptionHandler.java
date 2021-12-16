991f12d242502b20074131ffd3eee3bb0ebbb46c
Dasha-And <dosados69@gmail.com>
2021-12-16 21:37:13 +0200

    added rest exception handler

---
+++ src/main/java/wildlife/care/exceptions/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package wildlife.care.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(WrongPasswordException.class)
+    @ResponseBody
+    public ResponseEntity<Object> handleWrongPassword(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserIsNotRegistered.class)
+    @ResponseBody
+    public ResponseEntity<Object> handleUserNotRegistred(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(exception.getMessage());
+    }
+}
